a0e82607643f1d0e53348268c3bef619ed1ecdc7
Ruben Barkow-Kuder <github@r.z11.de>
2025-07-24 02:06:41 +0200

    fix: update PHP short tags to full tags globally

--- help.php
+++ help.php
@@ -5,7 +5,7 @@
 ?>
 <HTML>
 <HEAD>
-<TITLE>TIMEEFFECT - <? if(!empty($GLOBALS['_PJ_strings']['help'])) echo $GLOBALS['_PJ_strings']['help'] ?></TITLE>
+<TITLE>TIMEEFFECT - <?php if(!empty($GLOBALS['_PJ_strings']['help'])) echo $GLOBALS['_PJ_strings']['help'] ?></TITLE>
 <LINK REL="stylesheet" HREF="<?php print $_PJ_css_path; ?>/project.css" type="text/css">
 </head>
 
@@ -19,20 +19,20 @@
 		<TD CLASS="content" ALIGN="center">
 		<TABLE BORDER="0" CELLPADDING="0" CELLSPACING="0" WIDTH="90%">
 			<TR>
-				<TD><IMG src="<? if(!empty($GLOBALS['_PJ_image_path'])) echo $GLOBALS['_PJ_image_path'] ?>/abstand.gif" HEIGHT="20" WIDTH="1" BORDER="0"></TD>
+				<TD><IMG src="<?php if(!empty($GLOBALS['_PJ_image_path'])) echo $GLOBALS['_PJ_image_path'] ?>/abstand.gif" HEIGHT="20" WIDTH="1" BORDER="0"></TD>
 			</TR><TR>
-				<TD><IMG src="<? if(!empty($GLOBALS['_PJ_image_path'])) echo $GLOBALS['_PJ_image_path'] ?>/abstand.gif" HEIGHT="20" WIDTH="1" BORDER="0"></TD>
+				<TD><IMG src="<?php if(!empty($GLOBALS['_PJ_image_path'])) echo $GLOBALS['_PJ_image_path'] ?>/abstand.gif" HEIGHT="20" WIDTH="1" BORDER="0"></TD>
 			</TR><TR>
-				<TD><b><? if(!empty($GLOBALS['_PJ_strings']['not_implemented'])) echo $GLOBALS['_PJ_strings']['not_implemented'] ?>!</b></TD>
+				<TD><b><?php if(!empty($GLOBALS['_PJ_strings']['not_implemented'])) echo $GLOBALS['_PJ_strings']['not_implemented'] ?>!</b></TD>
 			</TR><TR>
-				<TD><IMG src="<? if(!empty($GLOBALS['_PJ_image_path'])) echo $GLOBALS['_PJ_image_path'] ?>/abstand.gif" HEIGHT="20" WIDTH="1" BORDER="0">
+				<TD><IMG src="<?php if(!empty($GLOBALS['_PJ_image_path'])) echo $GLOBALS['_PJ_image_path'] ?>/abstand.gif" HEIGHT="20" WIDTH="1" BORDER="0">
 				<HR SIZE="1"></TD>
 			</TR><TR>
-				<TD><? if(!empty($GLOBALS['_PJ_strings']['translations'])) echo $GLOBALS['_PJ_strings']['translations'] ?></TD>
+				<TD><?php if(!empty($GLOBALS['_PJ_strings']['translations'])) echo $GLOBALS['_PJ_strings']['translations'] ?></TD>
 			</TR><TR>
-				<TD><IMG src="<? if(!empty($GLOBALS['_PJ_image_path'])) echo $GLOBALS['_PJ_image_path'] ?>/abstand.gif" HEIGHT="40" WIDTH="1" BORDER="0"></TD>
+				<TD><IMG src="<?php if(!empty($GLOBALS['_PJ_image_path'])) echo $GLOBALS['_PJ_image_path'] ?>/abstand.gif" HEIGHT="40" WIDTH="1" BORDER="0"></TD>
 			</TR><TR>
-				<TD ALIGN="center"><A CLASS="note" HREF="JavaScript:self.close()"><IMG src="<? if(!empty($GLOBALS['_PJ_icon_path'])) echo $GLOBALS['_PJ_icon_path'] ?>/close_window.gif" HEIGHT="16" WIDTH="16" BORDER="0" ALIGN="absmiddle">&nbsp;<? if(!empty($GLOBALS['_PJ_strings']['close_window'])) echo $GLOBALS['_PJ_strings']['close_window'] ?></A></TD>
+				<TD ALIGN="center"><A CLASS="note" HREF="JavaScript:self.close()"><IMG src="<?php if(!empty($GLOBALS['_PJ_icon_path'])) echo $GLOBALS['_PJ_icon_path'] ?>/close_window.gif" HEIGHT="16" WIDTH="16" BORDER="0" ALIGN="absmiddle">&nbsp;<?php if(!empty($GLOBALS['_PJ_strings']['close_window'])) echo $GLOBALS['_PJ_strings']['close_window'] ?></A></TD>
 			</TR>
 		</TABLE>
 		</TD>
