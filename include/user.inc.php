9537bdcc4dabc1bb3cd9c6ad63ae96f5392443cc
Ruben Barkow-Kuder <github@r.z11.de>
2025-07-30 07:06:16 +0200

    feat: improve password handling with separate flows for new and existing users

--- include/user.inc.php
+++ include/user.inc.php
@@ -184,18 +184,29 @@ else return null;
 	        	return $GLOBALS['_PJ_strings']['error_user_exists'];
 	        }
 
+			// Password validation: only require password for new users or when changing password
+		if(empty($this->data['id'])) {
+			// New user: password is required
 			if($this->data['password'] == '') {
-	        	return $GLOBALS['_PJ_strings']['error_pw_empty'];
-	        }
-
-	        if($this->data['password'] != $GLOBALS['_PJ_password_dummy']) {
-	        	if($this->data['password'] != $this->data['password_retype']) {
-	        		return $GLOBALS['_PJ_strings']['error_pw_retype'];
-	        	}
-	        	$password = md5($this->data['password']);
-	        } else if($this->data['id']) {
+				return $GLOBALS['_PJ_strings']['error_pw_empty'];
+			}
+			$password = md5($this->data['password']);
+		} else {
+			// Existing user: only validate password if it's being changed
+			if($this->data['password'] != $GLOBALS['_PJ_password_dummy']) {
+				// Password is being changed
+				if($this->data['password'] == '') {
+					return $GLOBALS['_PJ_strings']['error_pw_empty'];
+				}
+				if($this->data['password'] != $this->data['password_retype']) {
+					return $GLOBALS['_PJ_strings']['error_pw_retype'];
+				}
+				$password = md5($this->data['password']);
+			} else {
+				// Password not being changed, keep existing password
 				$password = $this->retrieve($this->data['id'], 'password');
-	        }
+			}
+		}
 
 			if($this->data['lastname'] == '') {
 	        	return $GLOBALS['_PJ_strings']['error_name_empty'];
